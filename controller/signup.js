deee6e1808f1b1feddd7964ba7c02477bb20685d
shabbarrazaansari <shabbaransari98@gmail.com>
2023-12-13 12:26:39 +0530

    password encryption and decryption

--- controller/signup.js
+++ controller/signup.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const bcrypt = require('bcrypt');
 
 const user = require('../models/user');
 function ifStringValid(string) {
@@ -19,12 +20,17 @@ exports .signup = async(req,res,next)=>{
         }
 
    try {
-    const data = await user.create({
-        name:req.body.name,
-        email:req.body.email,
-        password:req.body.password
+    const saltRounds = 10;
+    bcrypt.hash(password,saltRounds,async (err,hash)=>{ 
+        const data = await user.create({
+        name:name,
+        email:email,
+        password:hash
     })
     res.status(201).json({user:data})
+    })
+   
+    
    } catch (error) {
     res.status(500).json(error)
    }
@@ -40,12 +46,17 @@ exports .loginExist = async (req,res,next)=>{
     await user.findAll({where:{email}})
     .then(user=>{
         console.log(user)
-        if(user[0].password ===password){
-           return res.status(200).json({success:true,message:"user login successfully"})
+       bcrypt.compare(password,user[0].password,(err,result)=>{
+        if(err){
+            throw new Error();
+        } if(result === true) {
+            return res.status(200).json({success:true,message:"user login successfully"})
         }
-        else{
+        else {
             return res.status(400).json({success:false,message:'password is incorrect'})
         }
+       })
+   
     })
     .catch((err)=>{
         return res.status(404).json({success:false,message:'user does not exist'})
