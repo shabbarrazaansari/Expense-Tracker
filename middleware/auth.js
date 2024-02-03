fa33e8448873a88745bfa24ba92a085f3f0d95fa
shabbarrazaansari <shabbaransari98@gmail.com>
2024-02-03 10:42:20 +0530

    added

--- middleware/auth.js
+++ middleware/auth.js
@@ -6,7 +6,7 @@ exports .authenticate = async (req,res,next)=>{
     try {
         const token = req.header('Authorization');
         console.log(token);
-        const checkUser = jwt.verify(token,'8090501210');
+        const checkUser = jwt.verify(token,process.env.TOKEN_SECRET);
         console.log(checkUser.userId);
         const user = await User.findByPk(checkUser.userId);
         req.user = user;
